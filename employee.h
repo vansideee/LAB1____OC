f0a4799bb9c7d7524c8edcf319296b153e00f460
vansideee <strakovichvanya@gmail.com>
2025-09-18 09:55:11 +0300

    remake employee.h
    перегрузка операторов ввода и вывода для бинарного файла + оператора = для тестов

--- employee.h
+++ employee.h
@@ -1,8 +1,30 @@
-// employee.h
+// Core/employee.h
 #pragma once
 
+#include <iostream>
+#include <fstream>
+
 struct employee {
-    int num;         // идентификационный номер
-    char name[10];   // имя
-    double hours;    // отработанные часы
+    int num;
+    char name[10];
+    double hours;
+
+    // Перегрузка оператора вывода 
+    friend std::ostream& operator<<(std::ostream& os, const employee& emp) {
+        os.write(reinterpret_cast<const char*>(&emp), sizeof(employee));
+        return os;
+    }
+
+    // Перегрузка оператора ввода
+    friend std::istream& operator>>(std::istream& is, employee& emp) {
+        is.read(reinterpret_cast<char*>(&emp), sizeof(employee));
+        return is;
+    }
+
+    
+    bool operator==(const employee& other) const {
+        return num == other.num && 
+               strcmp(name, other.name) == 0 && 
+               hours == other.hours;
+    }
 };
